bef83968f1bd79b9f6326eae676ee2857c9a979f
Agustin Alexander Alegre Morán <133085786+AgustinAlegre588@users.noreply.github.com>
2023-06-16 21:57:52 -0300

    n

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
 menu.addEventListener("click", function () {
-    navbar.classList.toggle("active")>
+    navbar.classList.toggle("active");
 });
 
 window.onscroll = () => {
     navbar.classList.remove("active");
-}
+};
